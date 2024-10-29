0bca4692dad42cf023b2b202f74a9cc8ff567ac1
Alzapata317 <alzapata317@gmail.com>
2024-10-28 23:49:11 -0400

    added an about page and more support for mobile

--- src/app/Components/Nav.tsx
+++ src/app/Components/Nav.tsx
@@ -1,9 +1,10 @@
 "use client"
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import Link from 'next/link';
 import Image from 'next/image'
 import { RiTwitterXLine } from "react-icons/ri";
 import { FaInstagram, FaTiktok } from "react-icons/fa";
+import { TbBrandPatreon } from "react-icons/tb";
 import { SiGofundme } from "react-icons/si";
 import { Divide } from 'hamburger-react';
 import { AppContext } from '../../../context/AppContext';
@@ -18,14 +19,40 @@ export default function Nav() {
 
     const { active, setActive } = context;
 
+    useEffect(() => {
+        if (showTabs) {
+            // Prevent scrolling
+            document.body.classList.add('overflow-hidden');
+        } else {
+            // Re-enable scrolling
+            document.body.classList.remove('overflow-hidden');
+        }
+
+        // Cleanup when component unmounts
+        return () => {
+            document.body.classList.remove('overflow-hidden');
+        };
+    }, [showTabs]);
+
   return (
     <div className='bg-black h-[20vh] flex relative'>
         <div className='w-1/5 p-8 flex flex-col pm-sm:hidden pm-md:flex gap-4'>
             <ul className='w-full gap-5 flex'>
-                <Link className='w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg' target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/starboundmanga/?igsh=Zm9vaXIxZWRna2U1&utm_source=qr"><FaInstagram className=' w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg'/></Link>
-                <Link className='w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg' target="_blank" rel="noopener noreferrer" href="https://www.tiktok.com/@star.bound.manga?_t=8lFccmQzg5O&_r=1"><FaTiktok className=' w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg'/></Link>
-                <Link className='w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg' target="_blank" rel="noopener noreferrer" href="https://x.com/starboundmanga?s=21&t=6l6Ll6G8x9_37zykGoqYyg"><RiTwitterXLine className=' w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg'/></Link>
-                <Link className='w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg' target="_blank" rel="noopener noreferrer" href="https://www.gofundme.com/f/manga-project-starbound?utm_campaign=p_lico+share-sheet&utm_content=share_ai_control&utm_medium=copy_link&utm_source=customer"><SiGofundme className=' w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg'/></Link>
+                <Link className='w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg' target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/starboundmanga/?igsh=Zm9vaXIxZWRna2U1&utm_source=qr">
+                    <FaInstagram className=' w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg'/>
+                </Link>
+                <Link className='w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg' target="_blank" rel="noopener noreferrer" href="https://www.tiktok.com/@star.bound.manga?_t=8lFccmQzg5O&_r=1">
+                    <FaTiktok className=' w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg'/>
+                </Link>
+                <Link className='w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg' target="_blank" rel="noopener noreferrer" href="https://x.com/starboundmanga?s=21&t=6l6Ll6G8x9_37zykGoqYyg">
+                    <RiTwitterXLine className=' w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg'/>
+                </Link>
+                <Link className='w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg' target="_blank" rel="noopener noreferrer" href="https://www.gofundme.com/f/manga-project-starbound?utm_campaign=p_lico+share-sheet&utm_content=share_ai_control&utm_medium=copy_link&utm_source=customer">
+                    <SiGofundme className=' w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg'/>
+                </Link>
+                <Link className='w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg' target="_blank" rel="noopener noreferrer" href="https://patreon.com/Star_Bound_Series?utm_medium=clipboard_copy&utm_source=copyLink&utm_campaign=creatorshare_creator&utm_content=join_link">
+                    <TbBrandPatreon className=' w-[2vw] pm-md:w-[3vw] h-[3.5vh] rounded-lg'/>
+                </Link>
             </ul>
             <a href="mailto:Star.bound.series.inc@gmail.com" className='w-full text-white hover:text-purple-400'>Star.bound.series.inc@gmail.com</a>
         </div>
@@ -35,18 +62,18 @@ export default function Nav() {
         <div className='w-3/5 flex flex-col justify-center items-center'>
             <div className='w-full h-4/5 flex items-center justify-center'>
             <Link href="/" className='w-1/2 pm-md:w-2/3 h-4/5 flex items-center justify-center pm-sm:hidden pm-md:flex'><Image src="/assets/images/DesktopLogo2.png" alt='Star-Bound Logo' width={1700} height={2400}/></Link>
-            <Link href="/" className='pm-sm:w-[20vw] pm-md:hidden h-3/5 flex items-center justify-center lm-sm:hidden'><Image src="/assets/images/MobileLogo1.png" alt='Star-Bound Logo' width={1700} height={2400}/></Link>
+            <Link href="/" className='pm-sm:w-[20vw] pm-md:hidden h-3/5 flex items-center justify-center lm-sm:hidden'><Image src="/assets/images/MobileLogo3.png" alt='Star-Bound Logo' width={1700} height={2400}/></Link>
             </div>
             <ul className='w-full h-1/5 flex items-center justify-center gap-[10vw] text-white text-[2vh] pm-sm:hidden pm-md:flex'>
-                <button className={`hover:border-b-2 hover:border-b-white hover:text-purple-400 ${active == "Home" ? 'text-purple-400 border-b-white border-b-2' : ''}`} onClick={() => (setActive("Home"))}>Home</button>
-                <button className={`hover:border-b-2 hover:border-b-white hover:text-purple-400 ${active == "News" ? 'text-purple-400 border-b-white border-b-2' : ''}`} onClick={() => (setActive("News"))}>News</button>
-                <button className={`hover:border-b-2 hover:border-b-white hover:text-purple-400 ${active == "Merch" ? 'text-purple-400 border-b-white border-b-2' : ''}`} onClick={() => (setActive("Merch"))}>Merch</button>
-                <button className={`hover:border-b-2 hover:border-b-white hover:text-purple-400 ${active == "About" ? 'text-purple-400 border-b-white border-b-2' : ''}`} onClick={() => (setActive("About"))}>About</button>
+                <button className={`hover:border-b-2 hover:border-b-white hover:text-purple-400 ${active == "Home" ? 'text-purple-400 border-b-white border-b-2' : ''}`} onClick={() => (setActive("Home"), setShowTabs(false))}>Home</button>
+                <button className={`hover:border-b-2 hover:border-b-white hover:text-purple-400 ${active == "News" ? 'text-purple-400 border-b-white border-b-2' : ''}`} onClick={() => (setActive("News"), setShowTabs(false))}>News</button>
+                <button className={`hover:border-b-2 hover:border-b-white hover:text-purple-400 ${active == "Merch" ? 'text-purple-400 border-b-white border-b-2' : ''}`} onClick={() => (setActive("Merch"), setShowTabs(false))}>Merch</button>
+                <button className={`hover:border-b-2 hover:border-b-white hover:text-purple-400 ${active == "About" ? 'text-purple-400 border-b-white border-b-2' : ''}`} onClick={() => (setActive("About"), setShowTabs(false))}>About</button>
             </ul>
         </div>
         <div className='w-1/5'></div>
         {showTabs == true && (
-            <div className='absolute top-[20vh] bg-black h-[80vh] w-full lm-sm:hidden pm-md:hidden'>
+            <div className='absolute top-[20vh] bg-black h-[85vh] w-full lm-sm:hidden pm-md:hidden z-10'>
                 <ul className='flex flex-col px-[15vw] py-[5vh] text-[2.5vh] gap-7 items-start'>
                     <button className='hover:text-purple-400' onClick={() => (setActive("Home"), setShowTabs(false))}>Home</button>
                     <button className='hover:text-purple-400' onClick={() => (setActive("News"), setShowTabs(false))}>News</button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from "../../context/AppContext";
+import Link from 'next/link';
 
 type ImageType = {
   id: string;
@@ -66,19 +67,21 @@ export default function Home() {
         <div className="w-full h-auto bg-cover" style={{ backgroundImage: `url(${"/assets/images/Background1.jpeg"})` }}>
           {/* Video Section */}
           {videoVisible && (
-            <div className={`w-full h-[80vh] overflow-hidden ${videoEnded ? 'animate-shrink' : ''}`}>
+            <div className={`w-full h-[80vh] overflow-hidden ${videoEnded ? 'animate-shrink' : ''} md:h-[60vh] sm:h-[50vh]`}>
               <video
                 src="/assets/videos/Animation.mp4"
                 autoPlay
                 muted
+                playsInline
                 className="w-full h-full object-cover"
                 onEnded={handleVideoEnd}
               />
             </div>
           )}
           {/* Rest of your content */}
-          <div className="w-full h-[80vh] flex items-center px-[10vw] py-[10vh] bg-[#00000039]">
-            <main className="w-2/6 h-[60vh] flex shadow-2xl">
+          <div className="w-full h-[80vh] pm-sm:h-auto flex pm-sm:flex-col items-center px-[10vw] py-[10vh] bg-[#00000039] relative">
+            <img className='absolute bottom-[2vh] right-0 w-[10vw] h-[18vh] pm-sm:w-[30vw]' src='/assets/images/art/Kallian1.png' />
+            <main className="w-2/6 pm-sm:w-5/6 h-[60vh] pm-sm:h-[50vh] flex shadow-2xl">
               {/* Main Image */}
               <img
                 className="bg-green-500 w-full flex items-center justify-center rounded-2xl"
@@ -86,7 +89,7 @@ export default function Home() {
                 alt="Main Image"
               />
               {/* Side Images */}
-              <div className="bg-pink-400 w-1/5 rounded-r-2xl flex flex-col">
+              <div className="bg-pink-400 w-1/5 rounded-r-2xl flex flex-col hidden">
                 {sideImages.map((image, index) => {
                   const isFirst = index === 0;
                   const isLast = index === sideImages.length - 1;
@@ -112,23 +115,61 @@ export default function Home() {
                 })}
               </div>
             </main>
-            <aside className='w-4/6 h-full flex flex-col items-center pl-[5vw]'>
-              <h2 className='h-[15vh] flex justify-center items-center font-bold text-purple-400'>Volume 1 of Star-Bound</h2>
-              <p className='text-[2.5vh]'>
+            <aside className='w-4/6 pm-sm:w-full h-full flex flex-col items-center pl-[5vw]'>
+              <h2 className='h-[15vh] flex justify-center items-center text-center font-bold text-purple-400 lm-sm:hidden text-[5vh]'>Volume 1</h2>
+              <h2 className='h-[15vh] flex justify-center items-center text-center font-bold text-purple-400 pm-sm:hidden'>Volume 1 of Star-Bound</h2>
+              <p className='text-[2.5vh] pm-sm:text-[2vh]'>
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
               </p>
-              <button 
-                className='w-[12vw] h-[6vh] bg-gradient-to-b hover:from-purple-600 hover:to-indigo-600 from-cyan-300 to-blue-600 text-[3vh] text-black rounded-2xl mt-[4vh]'
-              >
-                Buy Now!
-              </button>
+              <Link href='https://a.co/d/9Cn7nMX' target="_blank" rel="noopener noreferrer">
+                <button 
+                  className='w-[12vw] pm-sm:w-[35vw] h-[6vh] bg-gradient-to-b hover:from-purple-600 hover:to-indigo-600 from-cyan-300 to-blue-600 text-[3vh] text-black rounded-2xl mt-[4vh]'
+                >
+                  Buy Now!
+                </button>
+              </Link>
             </aside>
           </div>
         </div>
       )}
+      {active === "News" && (
+        <div className='w-full h-[80vh] bg-cover px-[10vw] py-[10vh]' style={{ backgroundImage: `url(${"/assets/images/Background1.jpeg"})` }}>
+          {/* <h1 className='w-full text-[6vh] text-center h-[15vh] pm-sm:text-[5vh]'>Recent Updates</h1>
+          <ul className='list-disc pm-sm:text-[2.5vh]'>
+            <li>Volume 1 of Star-Bound is released go check it out on Amazon!  -  October 27</li>
+          </ul> */}
+        </div>
+      )}
       {active === "Merch" && (
-        <div className='w-full h-[80vh] flex justify-center items-center'>
+        <div className='w-full h-[80vh] flex justify-center items-center bg-cover text-black text-[6vh] pm-sm:text-[4vh]' style={{ backgroundImage: `url(${"/assets/images/Background2.jpeg"})` }}>
           COMING SOON ....
+        </div>
+      )}
+      {active === "About" && (
+        <div className='w-full lm-sm:h-[80vh] px-[10vw] py-[5vh] bg-cover text-black' style={{ backgroundImage: `url(${"/assets/images/Background2.jpeg"})` }}>
+          <div className='flex w-full justify-between'>
+            <img 
+              src="/assets/images/art/Kallian2.png" 
+              alt=""
+              className='w-[10vw] pm-sm:w-[30vw] h-[17vh]' 
+            />
+            <img 
+              src="/assets/images/art/Kallian3.png" 
+              alt=""
+              className='w-[10vw] pm-sm:w-[30vw] h-[17vh] pm-sm:mt-[2vh]' 
+            />
+          </div>
+          <div className='text-[3vh] pm-sm:text-[2vh] text-center lm-sm:px-[15vw] w-full pm-sm:mt-[3vh]'>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+                esse cillum dolore eu fugiat nulla pariatur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+                esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <h2 className='text-[5vh] w-full text-center mt-[5vh]'>Enjoy the Story!</h2>
+          </div>
         </div>
       )}
     </div>
